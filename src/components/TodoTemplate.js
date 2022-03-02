8644de2a5f5bc5319caa742f4f73eb96665bc6b7
mjjingoo <ivg04155@gmail.com>
2022-03-02 22:54:03 +0900

    컴포넌트 생성

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoTemplateBlock = styled.div`
+    position: relative;
+    width: 512px;
+    height: 768px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    margin-top: 96px;
+    margin-bottom: 32px;
+    background: white;
+    border-radius: 16px;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+`;
+
+function TodoTemplate({ children }) {
+    return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+}
+
+export default TodoTemplate;
