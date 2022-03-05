374f0240fc90fe48c84d456b8984cf30ba82bcbc
mjjingoo <ivg04155@gmail.com>
2022-03-05 15:36:16 +0900

    할일갯수, 날짜 기능 추가

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -25,12 +25,21 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead() {
     const todos = useTodoState();
-    console.log(todos);
+    const undoneTasks = todos.filter(todo => !todo.done);
+
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR', { //년월일
+        year:'numeric',
+        month:'long',
+        day:'numeric'
+    });
+    const dayName = today.toLocaleDateString('ko-KR', {weekday:'long'}); //요일
+
     return (
     <TodoHeadBlock>
-        <h1>2022년 2월 22일</h1>
-        <div className="day">월요일</div>
-        <div className="tasks-left">할일 2개 남음</div>
+        <h1>{dateString}</h1>
+        <div className="day">{dayName}</div>
+        <div className="tasks-left">할일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
     );
 }
