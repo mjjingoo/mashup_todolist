b66e14ee02615f3e26bd681f4504dc75299a4e4e
mjjingoo <ivg04155@gmail.com>
2022-03-07 16:28:43 +0900

    enter 누를 시 리스트 등록 오류 수정

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import styled, {css} from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import {useTodoDispatch, useTodoNextId} from '../TodoContext';
 
 const CircleButton = styled.div`
     background:#38d9a9;
@@ -51,7 +52,7 @@ const InsertFormPositioner = styled.div`
     position:absolute;
 `;
 
-const InsertForm = styled.div`
+const InsertForm = styled.form`
     background: #f8f9fa;
     padding:32px 32px 72px;
     border-bottom-left-radius: 16px;
@@ -71,14 +72,40 @@ const Input = styled.input`
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
 
     const onToggle = () => setOpen(!open);
+    const onChange = e => setValue(e.target.value);
+    const onSubmit = e => {
+        console.log('aaa');
+        e.preventDefault();
+        dispatch ({
+            type : 'CREATE',
+            todo:{
+                id : nextId.current,
+                text : value,
+                done : false
+            }
+        });
+        setValue('');
+        setOpen(false);
+        nextId.current += 1;
+    };
+
     return (
         <>
             {open && (
                 <InsertFormPositioner>
-                    <InsertForm>
-                        <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+                    <InsertForm onSubmit={onSubmit}>
+                        <Input 
+                            autoFocus 
+                            placeholder="할 일을 입력 후, Enter를 누르세요"
+                            onChange={onChange}
+                            value={value} 
+                        />
                     </InsertForm>
                 </InsertFormPositioner>
             )}
@@ -89,4 +116,4 @@ function TodoCreate() {
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
